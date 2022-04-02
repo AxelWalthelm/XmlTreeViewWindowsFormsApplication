8ffd13b606e31b9585fe06506a040f7db0a51fcb
Axel <Axel@SILENT-PC>
2022-04-03 01:15:54 +0200

    Serializing data structure to XmlDocument and deserialize from XmlDocument.

--- XmlTreeViewWindowsFormsApplication/Form1.Designer.cs
+++ XmlTreeViewWindowsFormsApplication/Form1.Designer.cs
@@ -36,9 +36,11 @@
             this.insertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonWriteData = new System.Windows.Forms.Button();
             this.xmlTreeViewGeneric1 = new XmlTreeViewWindowsFormsApplication.XmlTreeViewGeneric();
             this.xmlTreeView2 = new XmlTreeViewWindowsFormsApplication.XmlTreeViewSimple();
             this.xmlTreeView1 = new XmlTreeViewWindowsFormsApplication.XmlTreeViewSimple();
+            this.buttonReadData = new System.Windows.Forms.Button();
             this.contextMenuStripXmlTreeView.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,7 +50,7 @@
             this.buttonLoad.Location = new System.Drawing.Point(13, 432);
             this.buttonLoad.Name = "buttonLoad";
             this.buttonLoad.Size = new System.Drawing.Size(75, 23);
-            this.buttonLoad.TabIndex = 2;
+            this.buttonLoad.TabIndex = 3;
             this.buttonLoad.Text = "Load";
             this.buttonLoad.UseVisualStyleBackColor = true;
             this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
@@ -59,7 +61,7 @@
             this.buttonSave.Location = new System.Drawing.Point(685, 432);
             this.buttonSave.Name = "buttonSave";
             this.buttonSave.Size = new System.Drawing.Size(75, 23);
-            this.buttonSave.TabIndex = 4;
+            this.buttonSave.TabIndex = 7;
             this.buttonSave.Text = "Save";
             this.buttonSave.UseVisualStyleBackColor = true;
             this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
@@ -70,7 +72,7 @@
             this.buttonReset.Location = new System.Drawing.Point(350, 432);
             this.buttonReset.Name = "buttonReset";
             this.buttonReset.Size = new System.Drawing.Size(75, 23);
-            this.buttonReset.TabIndex = 3;
+            this.buttonReset.TabIndex = 6;
             this.buttonReset.Text = "Reset";
             this.buttonReset.UseVisualStyleBackColor = true;
             this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
@@ -102,6 +104,17 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // buttonWriteData
+            // 
+            this.buttonWriteData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonWriteData.Location = new System.Drawing.Point(94, 432);
+            this.buttonWriteData.Name = "buttonWriteData";
+            this.buttonWriteData.Size = new System.Drawing.Size(75, 23);
+            this.buttonWriteData.TabIndex = 4;
+            this.buttonWriteData.Text = "Insert Data";
+            this.buttonWriteData.UseVisualStyleBackColor = true;
+            this.buttonWriteData.Click += new System.EventHandler(this.buttonWriteData_Click);
+            // 
             // xmlTreeViewGeneric1
             // 
             this.xmlTreeViewGeneric1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -110,7 +123,7 @@
             this.xmlTreeViewGeneric1.Location = new System.Drawing.Point(506, 13);
             this.xmlTreeViewGeneric1.Name = "xmlTreeViewGeneric1";
             this.xmlTreeViewGeneric1.Size = new System.Drawing.Size(253, 413);
-            this.xmlTreeViewGeneric1.TabIndex = 5;
+            this.xmlTreeViewGeneric1.TabIndex = 2;
             // 
             // xmlTreeView2
             // 
@@ -136,6 +149,17 @@
             this.xmlTreeView1.Size = new System.Drawing.Size(236, 413);
             this.xmlTreeView1.TabIndex = 0;
             // 
+            // buttonReadData
+            // 
+            this.buttonReadData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonReadData.Location = new System.Drawing.Point(174, 432);
+            this.buttonReadData.Name = "buttonReadData";
+            this.buttonReadData.Size = new System.Drawing.Size(75, 23);
+            this.buttonReadData.TabIndex = 5;
+            this.buttonReadData.Text = "Read Data";
+            this.buttonReadData.UseVisualStyleBackColor = true;
+            this.buttonReadData.Click += new System.EventHandler(this.buttonReadData_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -144,6 +168,8 @@
             this.Controls.Add(this.xmlTreeViewGeneric1);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonReadData);
+            this.Controls.Add(this.buttonWriteData);
             this.Controls.Add(this.buttonLoad);
             this.Controls.Add(this.xmlTreeView2);
             this.Controls.Add(this.xmlTreeView1);
@@ -168,6 +194,8 @@
         private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
         private XmlTreeViewGeneric xmlTreeViewGeneric1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonWriteData;
+        private System.Windows.Forms.Button buttonReadData;
     }
 }
 
--- XmlTreeViewWindowsFormsApplication/Form1.cs
+++ XmlTreeViewWindowsFormsApplication/Form1.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.XPath;
 
 namespace XmlTreeViewWindowsFormsApplication
 {
@@ -203,6 +205,162 @@ namespace XmlTreeViewWindowsFormsApplication
                 this.Text = $"XmlTreeViews - destruction timer finished";
                 this.timer1.Stop();
             }
+        }
+
+        [XmlType(TypeName = "faulty")]
+        public class TestData
+        {
+            public string Message;
+            public string Null;
+            public int ErrorCode;
+            public Point Loki { get; set; }
+        }
+
+        private void buttonWriteData_Click(object sender, EventArgs e)
+        {
+            var fault = new TestData
+            {
+                Message = "Exception occurred",
+                ErrorCode = 1010,
+                Loki = new Point(47, 11),
+            };
+
+            var name = nameof(fault);
+            var node = ((XmlTreeViewSimple.XmlTreeNode)this.xmlTreeView1.SelectedNode).XmlNode;
+            if (node.NodeType != XmlNodeType.Element || node.LocalName != name)
+            {
+                var n = XmlDocument.CreateElement(name);
+                node.AppendChild(n);
+                node = n;
+            }
+            SerializeObject(node, fault);
+            Console.WriteLine("Write Data: " + node.OuterXml);
+        }
+
+        private void buttonReadData_Click(object sender, EventArgs e)
+        {
+            var node = ((XmlTreeViewSimple.XmlTreeNode)this.xmlTreeView1.SelectedNode).XmlNode;
+            var o = DeseralizeObject(node, typeof(TestData));
+            Console.WriteLine("Read Data: " + o.ToString());
+        }
+
+        private static object DeseralizeObject(XmlNode xmlNode, Type type)
+        {
+            var overrides = new XmlAttributeOverrides();
+            overrides.Add(type, new XmlAttributes { XmlType = new XmlTypeAttribute(xmlNode.LocalName) });
+            XmlSerializer serializer = new XmlSerializer(type, overrides);
+            using (XmlReader reader = new XmlNodeReader(xmlNode))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
+
+        private static void SerializeObject(XmlNode xmlNode, object o)
+        {
+            var document = new XmlDocument();
+            var navigator = document.CreateNavigator();
+
+            using (XmlWriter writer = navigator.AppendChild())
+            {
+                var serializer = new XmlSerializer(o.GetType());
+                var namespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+                serializer.Serialize(writer, o, namespaces);
+            }
+
+            UpdateChildrenFromOtherDocument(xmlNode, document.FirstChild);
+        }
+
+        // Make oldNode have children equal to newNode, but modify only what has changed to avoid flickering,
+        // assuming order of nodes stay the same and that nodes can only be deleted and inserted
+        // (moving a node is both: delete and insert).
+        // Document of newNode differs from the document of oldNode, so we need to copy them by XmlDocument.ImportNode().
+        // When a node is different and needs be copied, all off its children are copied too.
+        // TODO/TBD: could we change nodes instead of replacing them?
+        private static void UpdateChildrenFromOtherDocument(XmlNode oldNode, XmlNode newNode)
+        {
+#if true // quick and simple
+            while (oldNode.ChildNodes.Count > 0)
+                oldNode.RemoveChild(oldNode.ChildNodes[0]);
+            for (int i = 0; i < newNode.ChildNodes.Count; i++)
+                oldNode.AppendChild(oldNode.OwnerDocument.ImportNode(newNode.ChildNodes[i], true));
+#else
+            var oldDocument = oldNode.OwnerDocument;
+
+            for (int index = 0; index < newNode.ChildNodes.Count; index++)
+            {
+                if (index >= oldNode.ChildNodes.Count)
+                {
+                    var importNode = oldDocument.ImportNode(newNode.ChildNodes[index], true);
+                    oldNode.AppendChild(importNode);
+                    continue;
+                }
+
+                // TODO/TBD: could we change nodes to make them equal?
+
+                if (IsEqual(oldNode.ChildNodes[index], newNode.ChildNodes[index]))
+                {
+                    UpdateChildrenFromOtherDocument(newNode.ChildNodes[index], oldNode.ChildNodes[index]);
+                    continue;
+                }
+
+                int oldIndex = IndexOf(oldNode.ChildNodes, newNode.ChildNodes[index], index + 1);
+                int newIndex = IndexOf(newNode.ChildNodes, oldNode.ChildNodes[index], index + 1);
+                if (newIndex >= 0 && (oldIndex < 0 || oldIndex > newIndex))
+                {
+                    // insert from newIndex
+                    var importNode = oldDocument.ImportNode(newNode.ChildNodes[newIndex], true);
+                    oldNode.InsertBefore(importNode, oldNode.ChildNodes[index]);
+                }
+                else
+                {
+                    // delete
+                    oldNode.RemoveChild(oldNode.ChildNodes[index]);
+                }
+            }
+
+            while (oldNode.ChildNodes.Count > newNode.ChildNodes.Count)
+            {
+                oldNode.RemoveChild(oldNode.ChildNodes[oldNode.ChildNodes.Count - 1]);
+            }
+#endif
+        }
+
+        private static int IndexOf(XmlNodeList list, XmlNode item, int startIndex = 0)
+        {
+            for (int i = startIndex; i < list.Count; i++)
+            {
+                if (IsEqual(list[i], item))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        private static bool IsEqual(XmlNode xmlNode1, XmlNode xmlNode2)
+        {
+            if (xmlNode1 == null || xmlNode2 == null)
+                return xmlNode1 == null && xmlNode2 == null;
+
+            if (xmlNode1.NodeType != xmlNode2.NodeType)
+                return false;
+
+            if (xmlNode1.NodeType == XmlNodeType.Element)
+            {
+                if (xmlNode1.Attributes.Count != xmlNode2.Attributes.Count)
+                    return false;
+
+                for (int i = 0; i < xmlNode1.Attributes.Count; i++)
+                {
+                    if (!IsEqual(xmlNode1.Attributes[i], xmlNode2.Attributes[i]))
+                        return false;
+                }
+            }
+
+            return
+                string.Compare(xmlNode1.Name, xmlNode2.Name, StringComparison.InvariantCulture) == 0 &&
+                string.Compare(xmlNode1.LocalName, xmlNode2.LocalName, StringComparison.InvariantCulture) == 0 &&
+                string.Compare(xmlNode1.Prefix, xmlNode2.Prefix, StringComparison.InvariantCulture) == 0 &&
+                string.Compare(xmlNode1.Value, xmlNode2.Value, StringComparison.InvariantCulture) == 0;
         }
     }
 }
